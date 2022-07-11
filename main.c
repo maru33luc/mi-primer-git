2d26a37f7ffddee84c74b1a0b6b32435a92a6b4b
Marina <maru33luc@gmail.com>
2022-07-11 18:38:23 -0300

    mi tercer commit

--- main.c
+++ main.c
@@ -5,9 +5,12 @@ int main()
 {
     printf("Hello world!\n");
 
-    printf("Hello world!\n");
-    printf("Hello world!\n");
-    printf("Hello world!\n");
+    int num=30;
+
+    printf("ingrese un numero\n");
+    scanf("%d", &num);
+
+    printf("%d", num);
 
 
 
